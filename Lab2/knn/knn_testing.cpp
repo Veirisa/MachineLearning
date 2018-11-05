b01e62b53769d7ad79e4e26ee296e29d5dd10cd8
Veirisa <anya-rodionova_all@mail.ru>
2018-11-05 19:47:03 +0300

    final knn

--- Lab2/knn/knn_testing.cpp
+++ Lab2/knn/knn_testing.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <cstdlib>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 typedef unsigned int uint;
@@ -65,6 +66,14 @@ struct score {
         double denominator = aver_precision + aver_recall;
         double aver_f = denominator == 0 ? 0 : 2 * aver_precision * aver_recall / denominator;
         return aver_f;
+    }
+
+    static double accuracy(uint k, uint n, const vector<vector<double>>& cm) {
+        double right = 0;
+        for (uint i = 0; i < k; ++i) {
+            right += cm[i][i];
+        }
+        return right / n;
     }
 
 };
@@ -126,6 +135,44 @@ struct knn {
         cout << "\n";
     }
 
+    string get_metric() {
+        switch (mc) {
+            case EUCLIDIAN:
+                return "EUCLIDIAN";
+            case MANTHATTAN:
+                return "MANTHATTAN";
+            default:
+                return "";
+        }
+    }
+
+    string get_kernel() {
+        switch (kl) {
+            case UNIFORM:
+                return "UNIFORM";
+            case TRIANGULAR:
+                return "TRIANGULAR";
+            case GAUSSIAN:
+                return "GAUSSIAN";
+            case EPANECHNIKOV:
+                return "EPANECHNIKOV";
+            case QUARTIC:
+                return "QUARTIC";
+            case TRIWEIGHT:
+                return "TRIWEIGHT";
+            default:
+                return "";
+        }
+    }
+
+    string get_neighb() {
+        return to_string(neighb);
+    }
+
+    string get_wind_ch() {
+        return to_string(wind_ch);
+    }
+
 private:
 
     uint m, k, n;
@@ -135,7 +182,7 @@ private:
     kernel kl;
     bool is_width;
     uint neighb;
-    double wind;
+    double wind, wind_ch;
 
     static bool cmp_pair(const pair<double, uint>& a, const pair<double, uint>& b) {
         return a.first < b.first;
@@ -238,6 +285,7 @@ private:
         bool best_is_width = is_width;
         uint best_neighb = neighb;
         double best_wind = wind;
+        double best_wind_ch = wind_ch;
         double best_score = 0;
         double score;
         vector<vector<double>> cm;
@@ -270,9 +318,11 @@ private:
                     if (is_width) {
                         neighb = 0;
                         wind = get_width(winds[iw]);
+                        wind_ch = winds[iw];
                     } else {
                         neighb = neighbs[in];
                         wind = 0;
+                        wind_ch = 0;
                     }
                     for (uint i = 0; i < k; ++i) {
                         for (uint j = 0; j < k; ++j) {
@@ -290,9 +340,11 @@ private:
                         if (is_width) {
                             best_neighb = 0;
                             best_wind = wind;
+                            best_wind_ch = wind_ch;
                         } else {
                             best_neighb = neighb;
                             best_wind = 0;
+                            best_wind_ch = 0;
                         }
                         best_score = score;
                     }
@@ -304,9 +356,122 @@ private:
         is_width = best_is_width;
         neighb = best_neighb;
         wind = best_wind;
+        wind_ch = best_wind_ch;
     }
 
 };
+
+
+// Testing
+
+void generate_max_test() {
+    ofstream fout("tests/08");
+    fout << 200 << " " << 25 << " " << 400 << "\n";
+    for (uint i = 0; i < 400; ++i) {
+        fout << 2 * i + (i / 16 + 1) << " ";
+        for (uint j = 0; j < 199; ++j) {
+            fout << 0 << " ";
+        }
+        fout << i / 16 + 1 << "\n";
+    }
+    fout << 400 << "\n";
+    for (uint i = 0; i < 400; ++i) {
+        fout << 2 * i + (i / 16 + 1) + 1 << " ";
+        for (uint j = 0; j < 199; ++j) {
+            fout << 0 << " ";
+        }
+        fout << "\n";
+    }
+    ofstream fout_a("tests/08.a");
+    for (uint i = 0; i < 400; ++i) {
+        fout_a << i / 16 + 1 << "\n";
+    }
+    fout_a << 1;
+}
+
+void testing() {
+    for (uint test = 1; test <= 8; ++test) {
+
+        clock_t t = clock();
+
+        cout << "----- test" << test << " -----" << "\n";
+        ifstream fin("tests/0" + to_string(test));
+        ifstream fin_ans("tests/0" + to_string(test) + ".a");
+
+        uint m, k, n;
+        fin >> m >> k >> n;
+
+        vector<double> min_v(m, 1000000000);
+        vector<double> max_v(m, 0);
+
+        vector<object> data;
+        vector<double> a(m, 0);
+        uint c;
+        for (uint i = 0; i < n; ++i) {
+            for (uint j = 0; j < m; ++j) {
+                fin >> a[j];
+                min_v[j] = min(min_v[j], a[j]);
+                max_v[j] = max(max_v[j], a[j]);
+            }
+            fin >> c;
+            data.push_back({i + 1, a, c - 1});
+        }
+
+        vector<vector<double>> cm;
+        cm.resize(k);
+        for (uint i = 0; i < k; ++i) {
+            cm[i].resize(k);
+            for (uint j = 0; j < k; ++j) {
+                cm[i][j] = 0;
+            }
+        }
+
+        uint q;
+        fin >> q;
+
+        uint c_ans[q];
+
+        vector<object> tests;
+        vector<double> b(m, 0);
+        for (uint i = 0; i < q; ++i) {
+            for (uint j = 0; j < m; ++j) {
+                fin >> b[j];
+                min_v[j] = min(min_v[j], b[j]);
+                max_v[j] = max(max_v[j], b[j]);
+            }
+            fin_ans >> c_ans[i];
+            tests.push_back({i + 1, b, 0});
+        }
+
+        for (uint j = 0; j < m; ++j) {
+            max_v[j] -= min_v[j];
+            if (max_v[j] > 0) {
+                for (uint i = 0; i < n; ++i) {
+                    data[i].f[j] -= min_v[j];
+                    data[i].f[j] /= max_v[j];
+                }
+                for (uint i = 0; i < q; ++i) {
+                    tests[i].f[j] -= min_v[j];
+                    tests[i].f[j] /= max_v[j];
+                }
+            }
+        }
+
+        knn solver{m, k, n, data};
+
+        for (uint i = 0; i < q; ++i) {
+            ++cm[c_ans[i] - 1][solver.predict(tests[i])];
+        }
+
+        double f, f_ans;
+        f = score::micro_f1(k, q, cm);
+        fin_ans >> f_ans;
+        cout << "have: " << f << "   ans: " << f_ans << "\n";
+        cout << solver.get_metric() << " - " << solver.get_kernel() << " - "
+             << solver.get_neighb() << " - " << solver.get_wind_ch() << "\n";
+        cout << "(time = " << (double)(clock() - t) / CLOCKS_PER_SEC << ")\n\n";
+    }
+}
 
 
 // Main
@@ -316,58 +481,8 @@ int main() {
     ios_base::sync_with_stdio(0);
     srand((time(0)));
 
-    uint m, k, n;
-    cin >> m >> k >> n;
-
-    vector<double> min_v(m, 1000000000);
-    vector<double> max_v(m, 0);
-
-    vector<object> data;
-    vector<double> a(m, 0);
-    uint c;
-    for (uint i = 0; i < n; ++i) {
-        for (uint j = 0; j < m; ++j) {
-            cin >> a[j];
-            min_v[j] = min(min_v[j], a[j]);
-            max_v[j] = max(max_v[j], a[j]);
-        }
-        cin >> c;
-        data.push_back({i + 1, a, c - 1});
-    }
-
-    uint q;
-    cin >> q;
-
-    vector<object> tests;
-    vector<double> b(m, 0);
-    for (uint i = 0; i < q; ++i) {
-        for (uint j = 0; j < m; ++j) {
-            cin >> b[j];
-            min_v[j] = min(min_v[j], b[j]);
-            max_v[j] = max(max_v[j], b[j]);
-        }
-        tests.push_back({i + 1, b, 0});
-    }
-
-    for (uint j = 0; j < m; ++j) {
-        max_v[j] -= min_v[j];
-        if (max_v[j] > 0) {
-            for (uint i = 0; i < n; ++i) {
-                data[i].f[j] -= min_v[j];
-                data[i].f[j] /= max_v[j];
-            }
-            for (uint i = 0; i < q; ++i) {
-                tests[i].f[j] -= min_v[j];
-                tests[i].f[j] /= max_v[j];
-            }
-        }
-    }
-
-    knn solver{m, k, n, data};
-    for (uint i = 0; i < q; ++i) {
-        solver.write_predict_info(tests[i]);
-        //cout << "class: " << solver.predict(tests[i]) + 1 << "\n";
-    }
+    //generate_max_test();
+    testing();
 
     return 0;
 }
